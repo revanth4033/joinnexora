4fa1d0de939f97c795d24d77678babd5f25091dd
Revanth Banisetti <revanthbanisetti403@gmail.com>
2025-07-27 17:37:35 +0530

    Fix database configuration to use DATABASE_URL

--- backend/config/database.js
+++ backend/config/database.js
@@ -17,11 +17,7 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     logging: false,
     pool: {
